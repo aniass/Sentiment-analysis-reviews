bec3509c4b0c52b1d4004cfed675b45e2af6f4ae
Aniass <a.staskiewicz4@gmail.com>
2022-07-05 18:19:55 +0200

    code refactor

--- clean_data.py
+++ clean_data.py
@@ -11,22 +11,25 @@ clothes = ['dress', 'color', 'wear', 'top', 'sweater', 'material', 'shirt',
 lem = WordNetLemmatizer()
 
 
+URL_DATA  = 'data\review_final.csv'
+
+
 def clean_text(words):
-    """The function to clean text"""
+    """Function to clean text"""
     words = re.sub("[^a-zA-Z]", " ", words)
     text = words.lower().split()
     return " ".join(text)
 
 
 def remove_stopwords(review):
-    """The function to removing stopwords"""
+    """Function to removing stopwords"""
     text = [word.lower() for word in review.split() if word.lower() not in
             stop_words and word.lower() not in clothes]
     return " ".join(text)
 
 
 def remove_numbers(text):
-    """The function to removing all numbers"""
+    """Function to removing all numbers"""
     new_text = []
     for word in text.split():
         if not re.search('\\d', word):
@@ -35,25 +38,24 @@ def remove_numbers(text):
 
 
 def get_lemmatize(text):
-    """The function to apply lemmatizing"""
+    """Function to apply lemmatizing"""
     lem_text = [lem.lemmatize(word) for word in text.split()]
     return " ".join(lem_text)
 
 
-# Load dataset
-url = 'C:\\Python Scripts\\NLP_projekty\\review_final.csv'
-dataset = pd.read_csv(url, header=0, index_col=0)
+def read_data(path):
+    """Function to read and clean text data """
+    data = pd.read_csv(path, header=0, index_col=0)
+    data['Review'] = data['Review'].astype(str)
+    data['Review'] = data['Review'].apply(clean_text)
+    data['Review'] = data['Review'].apply(remove_stopwords)
+    data['Review'] = data['Review'].apply(remove_numbers)
+    data['Review'] = data['Review'].apply(get_lemmatize)
+    return data
 
-# shape
-print(dataset.shape)
-print(dataset.head(5))
 
-dataset['Review'] = dataset['Review'].astype(str)
-dataset['Review'] = dataset['Review'].apply(clean_text)
-dataset['Review'] = dataset['Review'].apply(remove_stopwords)
-dataset['Review'] = dataset['Review'].apply(remove_numbers)
-dataset['Review'] = dataset['Review'].apply(get_lemmatize)
-print(dataset[:5])
-
-data = dataset.to_csv('C:\\Python Scripts\\NLP_projekty\\review_clean.csv',
-                      encoding='utf-8')
+if __name__ == '__main__':
+    dataset = read_data(URL_DATA)
+    print(dataset.shape)
+    print(dataset[:5])
+    dataset.to_csv('data\review_clean.csv',encoding='utf-8')

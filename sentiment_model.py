d99ffdec0b4b74042c93cf1754d74a8576bb8344
Aniass <a.staskiewicz4@gmail.com>
2023-09-11 17:02:20 +0200

    code refactor

--- sentiment_model.py
+++ sentiment_model.py
@@ -13,6 +13,7 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 
+
 stop_words = stopwords.words('english')
 clothes = ['dress', 'color', 'wear', 'top', 'sweater', 'material', 'shirt',
            'jeans', 'pant', 'skirt', 'order', 'white', 'black', 'fabric',
@@ -36,13 +37,13 @@ def read_data(path):
     """ Function to read and clean text data"""
     data = pd.read_csv(path, header=0, index_col=0)
     data['Review'] = data['Review'].apply(text_preprocess)
+    return data
+
+
+def prepare_data(data):
+    """ Function to split data on train and test set """
     X = data['Review']
     y = data['Recommended']
-    return X, y
-
-
-def prepare_data(X, y):
-    """ Function to split data on train and test set """
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25,
                                                         random_state=42)
     return X_train, X_test, y_train, y_test
@@ -77,6 +78,6 @@ def calculate_models(X_train, X_test, y_train, y_test):
 
 
 if __name__ == '__main__':
-    X, y = read_data(URL_DATA)
-    X_train, X_test, y_train, y_test = prepare_data(X, y)
+    df = read_data(URL_DATA)
+    X_train, X_test, y_train, y_test = prepare_data(df)
     calculate_models(X_train, X_test, y_train, y_test)

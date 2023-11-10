a4b97ced85cd935bfd7c34d5878076f7a986b59e
Aniass <a.staskiewicz4@gmail.com>
2023-11-10 19:19:42 +0100

    adding preprocess function

--- clean_data.py
+++ clean_data.py
@@ -4,14 +4,14 @@ import re
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 
-stop_words = stopwords.words('english')
-clothes = ['dress', 'color', 'wear', 'top', 'sweater', 'material', 'shirt',
-           'jeans', 'pant', 'skirt', 'order', 'white', 'black', 'fabric',
-           'blouse', 'sleeve', 'even', 'jacket']
-lem = WordNetLemmatizer()
-
 
 URL_DATA  = 'data\review_final.csv'
+
+
+def read_data(path):
+    """Function to read and data """
+    data = pd.read_csv(path, header=0, index_col=0)
+    return data
 
 
 def clean_text(words):
@@ -23,6 +23,10 @@ def clean_text(words):
 
 def remove_stopwords(review):
     """Function to removing stopwords"""
+    stop_words = stopwords.words('english')
+    clothes = ['dress', 'color', 'wear', 'top', 'sweater', 'material', 'shirt',
+           'jeans', 'pant', 'skirt', 'order', 'white', 'black', 'fabric',
+           'blouse', 'sleeve', 'even', 'jacket']
     text = [word.lower() for word in review.split() if word.lower() not in
             stop_words and word.lower() not in clothes]
     return " ".join(text)
@@ -39,13 +43,13 @@ def remove_numbers(text):
 
 def get_lemmatize(text):
     """Function to apply lemmatizing"""
+    lem = WordNetLemmatizer()
     lem_text = [lem.lemmatize(word) for word in text.split()]
     return " ".join(lem_text)
 
 
-def read_data(path):
-    """Function to read and clean text data """
-    data = pd.read_csv(path, header=0, index_col=0)
+def preprocess_data(data: str) -> str:
+    ''' Function to preprocess data'''
     data['Review'] = data['Review'].astype(str)
     data['Review'] = data['Review'].apply(clean_text)
     data['Review'] = data['Review'].apply(remove_stopwords)
@@ -55,7 +59,8 @@ def read_data(path):
 
 
 if __name__ == '__main__':
-    dataset = read_data(URL_DATA)
+    data = read_data(URL_DATA)
+    dataset = preprocess_data(data)
     print(dataset.shape)
     print(dataset[:5])
     dataset.to_csv('data\review_clean.csv',encoding='utf-8')

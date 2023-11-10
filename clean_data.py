e105ea22b0754d7a67c520a1e5e14b7d359a84a8
Aniass <a.staskiewicz4@gmail.com>
2023-11-10 19:26:56 +0100

    adding type hints

--- clean_data.py
+++ clean_data.py
@@ -8,20 +8,29 @@ from nltk.stem.wordnet import WordNetLemmatizer
 URL_DATA  = 'data\review_final.csv'
 
 
-def read_data(path):
+def read_data(path: str) -> pd.DataFrame:
     """Function to read and data """
     data = pd.read_csv(path, header=0, index_col=0)
     return data
 
 
-def clean_text(words):
+def clean_text(words: str) -> str:
     """Function to clean text"""
     words = re.sub("[^a-zA-Z]", " ", words)
     text = words.lower().split()
     return " ".join(text)
 
 
-def remove_stopwords(review):
+def remove_numbers(text: str) -> str:
+    """Function to removing all numbers"""
+    new_text = []
+    for word in text.split():
+        if not re.search('\\d', word):
+            new_text.append(word)
+    return ' '.join(new_text)
+
+
+def remove_stopwords(review: str) -> str:
     """Function to removing stopwords"""
     stop_words = stopwords.words('english')
     clothes = ['dress', 'color', 'wear', 'top', 'sweater', 'material', 'shirt',
@@ -32,16 +41,7 @@ def remove_stopwords(review):
     return " ".join(text)
 
 
-def remove_numbers(text):
-    """Function to removing all numbers"""
-    new_text = []
-    for word in text.split():
-        if not re.search('\\d', word):
-            new_text.append(word)
-    return ' '.join(new_text)
-
-
-def get_lemmatize(text):
+def get_lemmatize(text: str) -> str:
     """Function to apply lemmatizing"""
     lem = WordNetLemmatizer()
     lem_text = [lem.lemmatize(word) for word in text.split()]
@@ -52,8 +52,8 @@ def preprocess_data(data: str) -> str:
     ''' Function to preprocess data'''
     data['Review'] = data['Review'].astype(str)
     data['Review'] = data['Review'].apply(clean_text)
-    data['Review'] = data['Review'].apply(remove_stopwords)
     data['Review'] = data['Review'].apply(remove_numbers)
+    data['Review'] = data['Review'].apply(remove_stopwords)
     data['Review'] = data['Review'].apply(get_lemmatize)
     return data
 
